c8010735a88b51a6de270a9282bc50b930ac4b98
IzykPringle <ijpringl@mtu.edu>
2023-04-05 14:25:28 -0600

    link backend to server with get request

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+const { default: knex } = require('knex');
+const knexfile = require('./knexfile.js');
+const environment = process.env.NODE_ENV || 'development';
+const config = knexfile[environment];
+const db = knex(config);
 const port = 8080;
 
+app.use(cors());
 
+app.get('/', (req, res) => {
+    db
+    .select('*')
+    .from('items')
+    .then(data => res.json(data))
+})
 
 
 
